2a82d531b1b57bad15c781f3fcf3d69190c7472c
Dmytro Serhiienko <conneciones@gmail.com>
2026-03-02 00:04:04 +0200

    fix params axios

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -8,10 +8,13 @@ interface MovieResponse {
 const API_TOKEN = import.meta.env.VITE_TMDB_TOKEN;
 export async function fetchMovies(query: string): Promise<Movie[]> {
   const response = await axios.get<MovieResponse>(
-    `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`,
+    "https://api.themoviedb.org/3/search/movie",
     {
       params: {
-        // твої параметри...
+        query: query,
+        include_adult: false,
+        language: "en-US",
+        page: 1,
       },
       headers: {
         Authorization: `Bearer ${API_TOKEN}`,
